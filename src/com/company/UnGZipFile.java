a71196ca68f925355465cc156720eb27faaee21e
a.tkachuk <ast@loyverse.com>
2017-12-29 19:26:28 +0200

    добавили метод выборки айпишников

--- src/com/company/UnGZipFile.java
+++ src/com/company/UnGZipFile.java
@@ -17,26 +17,32 @@ public class UnGZipFile {
 
     private static final String FILENAME = "D:\\LOGS\\12-2017\\parseDSREQUEST\\" +
             "dsrequest.log.2017-12-01--*";
-
+    private static final String ipFILENAME = "D:\\LOGS\\12-2017\\" + "ip.txt";
+    private static final String resultFILENAME = "D:\\LOGS\\12-2017\\resultIP\\" +
+            "resultIP";
     private static final int BUFFER_SIZE = 1024;
+    private  static Map<String,String> listOwnerIP = new HashMap<String,String>(){{
+        put("0000",  "0.0.0.0");
+    }};
     Map<String, BufferedWriter> nameOfFiles = new HashMap<>();
-    Map<Integer, String> listOwnerIP = new HashMap<>();
 
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         //Map<String, FileWriter> nameOfFiles = new HashMap<String, FileWriter>();
         long start = System.currentTimeMillis();
-        UnGZipFile app = new UnGZipFile();
+        fileParser(ipFILENAME);
+ //       UnGZipFile app = new UnGZipFile();
         String fileDir = "D:\\LOGS\\12-2017\\parseDSREQUEST";
+/*
         Stream<Path> files = Files.list(Paths.get(fileDir));
         files.forEach(file -> {
             long startOfFile = System.currentTimeMillis();
-                  app.unGZIP(file.toString());
-            System.out.println("Время обработки файла "+ (System.currentTimeMillis()-startOfFile)/1000 + " секунд");
-            System.out.println("Время от начала работы "+ (System.currentTimeMillis()-start)/1000 + " секунд");
-
-            //System.out.println(file.toString());
-        });
-       app.closeAllFiles();
+            app.unGZIP(file.toString());
+            System.out.println("Время обработки файла " + (System.currentTimeMillis() - startOfFile) / 1000 + " секунд");
+            System.out.println("Время от начала работы " + (System.currentTimeMillis() - start) / 1000 + " секунд");
+       });
+*/
+   //     app.closeAllFiles();
 
         //app.unGZIP(FILENAME);
 
@@ -51,21 +57,21 @@ public class UnGZipFile {
      *
      * @param filename имя файла в котором находиться архив
      */
-    private void unGZIP(final String filename) {
+ /*   private void unGZIP(final String filename) {
         System.out.println("обрабатываем " + filename);
-
         try {
             GZIPInputStream gzipis = new GZIPInputStream(new FileInputStream(filename));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(gzipis) /*FileReader(FILENAME)*/);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(gzipis) *//*FileReader(FILENAME)*//*);
+
+
             String line;
             //WriteDataInFileWithNameOfOwner writeData = new WriteDataInFileWithNameOfOwner();
-            int lineCounter=0;
+            int lineCounter = 0;
             while ((line = reader.readLine()) != null) {
                 lineCounter++;
-                if (line.equals("\t")){
+                if (line.equals("\t")) {
                     continue;
                 }
-                //System.out.println(line);
                 try {
                     //String newline = ReadingStringFromFile.getOwnerDateCmd(line);//из процесса по генерации онеров
 
@@ -75,14 +81,65 @@ public class UnGZipFile {
                     }
                     //writeData.saveLine(newline);
                     //saveLine(newline); //из процесса по генерации онеров
-                    listOfOwnerIP.p
-                    add_IP_to_File(newline);
-
-                }catch (IndexOutOfBoundsException e){
-                    System.out.println("Error on line "+ line);
+                    String keyOwner = newline.substring(0, newline.indexOf("\t"));
+                    String ip = newline.substring(newline.indexOf("\t") + 1, newline.length());
+                    if (listOwnerIP.containsKey(keyOwner)){
+                        continue;
+                    }
+                    listOwnerIP.put(keyOwner, ip);
+                    progressBar(lineCounter, keyOwner);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Error on line " + line);
                 }
             }
+            add_IP_to_File(listOwnerIP);
             gzipis.close();
+            //закрываем все файлы
+            //closeAllFiles();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(UnGZipFile.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(UnGZipFile.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+    }*/
+    private static void fileParser(final String filename) {
+        System.out.println("обрабатываем " + filename);
+        try {
+            InputStream is = new FileInputStream(filename);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is) /*FileReader(FILENAME)*/);
+
+
+            String line;
+            //WriteDataInFileWithNameOfOwner writeData = new WriteDataInFileWithNameOfOwner();
+            int lineCounter = 0;
+            while ((line = reader.readLine()) != null) {
+                lineCounter++;
+                if (line.equals("\t")) {
+                    continue;
+                }
+                try {
+                    String newline = ReadingStringFromFile.getOwnerIP(line);
+                    if (newline.contains("##$$@@")) {
+                        continue;
+                    }
+                    //writeData.saveLine(newline);
+                    //saveLine(newline); //из процесса по генерации онеров
+                    String keyOwner = newline.substring(0, newline.indexOf("\t"));
+                    String ip = newline.substring(newline.indexOf("\t") + 1, newline.length());
+                    if (listOwnerIP.containsKey(keyOwner)){
+                        continue;
+                    }
+                    listOwnerIP.put(keyOwner, ip);
+                    progressBar(lineCounter, keyOwner);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Error on line " + line);
+                }
+            }
+            add_IP_to_File(listOwnerIP);
+            is.close();
+            //gzipis.close();
             //закрываем все файлы
             //closeAllFiles();
         } catch (FileNotFoundException ex) {
@@ -94,7 +151,19 @@ public class UnGZipFile {
         }
     }
 
-    private  void closeAllFiles() throws IOException {
+    public static void progressBar(int myCounter, String str) {
+        try {
+            String data = "\r" + "|/-\\".charAt(myCounter % "|/-\\".length()) + " обрабатываем строку " + myCounter + " с данными " + str;
+            System.out.write(data.getBytes());
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeAllFiles() throws IOException {
         for (BufferedWriter writer : nameOfFiles.values()) {
             writer.close();
         }
@@ -149,8 +218,26 @@ public class UnGZipFile {
         }
     }
 
+    public static void add_IP_to_File(Map<String, String> listOwnerIP) {
+        //вычленяем значение IP и запысываем в файл
+        String ip;
 
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(resultFILENAME , true));
+            for (Map.Entry entry : listOwnerIP.entrySet()) {
+                ip = entry.getValue().toString();
+                writer.write(ip);
+                writer.append('\n');
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage() + "\n" + ex.getCause());
+            ex.printStackTrace();
+            System.out.println();
+        }
+    }
 }
+
+
 
 
 
